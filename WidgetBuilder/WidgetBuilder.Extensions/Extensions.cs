9ce87f18b989781f1d6c4efcf12229eaf21e442f
Kevin Giszewski <kevin@giszewski.com>
2014-02-23 07:12:06 -0500

    Add WB Extensions and bump versions

---
+++ WidgetBuilder/WidgetBuilder.Extensions/Extensions.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using Umbraco.Core.Models;
+using Umbraco.Core;
+using Umbraco.Web;
+
+namespace WidgetBuilder.Extensions
+{
+    public static class Extensions
+    {
+        public static XmlDocument GetWidgetAsXml(IPublishedContent content, string propertyName)
+        {
+            var xd = new XmlDocument();
+
+            if (content == null)
+                return xd;
+
+            var rawXML = content.GetPropertyValue<string>(propertyName);
+
+            if (String.IsNullOrEmpty(rawXML))
+                return xd;
+
+            xd.LoadXml(rawXML);
+
+            return xd;
+        }
+
+        public static XmlNodeList GetAllWidgets(this IPublishedContent content, string propertyName)
+        {
+            var xd = GetWidgetAsXml(content, propertyName);
+
+            return xd.SelectNodes("//widget");
+        }
+
+        public static XmlNode GetWidget(this IPublishedContent content, string propertyName)
+        {
+            var xd = GetWidgetAsXml(content, propertyName);
+
+            return xd.SelectSingleNode("//widget");
+        }
+
+        public static string GetWidgetPropertyValue(this IPublishedContent content, string propertyName, string elementName)
+        {
+            var xd = GetWidgetAsXml(content, propertyName);
+            
+            var node = xd.SelectSingleNode("//widget/" + elementName);
+
+            if (node == null)
+                return "";
+
+            return node.InnerText;
+        }
+
+        public static List<string> GetWidgetPropertyValues(this IPublishedContent content, string propertyName, string elementName)
+        {
+            var xd = GetWidgetAsXml(content, propertyName);
+            var list = new List<string>();
+
+            var nodes = xd.SelectNodes("//widget/" + elementName);
+
+            if (nodes == null)
+                return list;
+
+            foreach (XmlNode node in nodes)
+            {
+                list.Add(node.InnerText);
+            }
+
+            return list;
+        }
+
+        public static string GetWidgetValue(this XmlNode widget, string elementName)
+        {
+            if (widget == null)
+                return "";
+
+            var element = widget.SelectSingleNode(elementName);
+
+            if (element == null)
+                return "";
+
+            return element.InnerText;
+        }
+
+        public static bool GetWidgetValue<T>(this XmlNode widget, string elementName)
+        {
+            if (widget == null)
+                return false;
+
+            var element = widget.SelectSingleNode(elementName);
+
+            if (element == null)
+                return false;
+
+            if (element.InnerText == "1" || element.InnerText.ToLower() == "true")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static string GetWidgetPropertyValueRaw(this XmlNode widget, string elementName)
+        {
+            if (widget == null)
+                return "";
+
+            var element = widget.SelectSingleNode(elementName);
+
+            if (element == null)
+                return "";
+
+            return element.InnerXml;
+        }
+
+        public static bool HasWidgetValue(this XmlNode widget, string elementName)
+        {
+            if (widget == null)
+                return false;
+
+            var element = widget.SelectSingleNode(elementName);
+
+            if (element == null)
+                return false;
+
+            return element.InnerText != "";
+        }
+
+        public static List<ListItem> ToListItems(this XmlDocument xd)
+        {
+            var listItems = new List<ListItem>();
+
+            foreach(XmlNode listItem in xd.SelectNodes("list/item"))
+            {
+                if (!String.IsNullOrEmpty(listItem.InnerText))
+                {
+                    listItems.Add(new ListItem() { Indent = Convert.ToInt32(listItem.Attributes["indent"].Value), Display = listItem.InnerText });
+                }
+            }
+
+            return listItems;
+        }
+    }
+
+    public class ListItem
+    {
+        public int Indent { get; set; }
+        public string Display { get; set; }
+    }
+}
--- WidgetBuilder/WidgetBuilder.Extensions/Properties/AssemblyInfo.cs
+++ WidgetBuilder/WidgetBuilder.Extensions/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DampWidgetExtension")]
+[assembly: AssemblyTitle("WidgetBuilder.Extensions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DampWidgetExtension")]
+[assembly: AssemblyProduct("WidgetBuilder.Extensions")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0308f8d6-f29a-42b0-bf2d-7b6aab789edb")]
+[assembly: Guid("b2e4b250-06ec-4842-8ea7-f191f625981f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
